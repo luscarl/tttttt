be14e3b17b6a5093625d5a39a67c8bf4362609c5
Lucy Lu <52309716+luscarl@users.noreply.github.com>
2024-02-06 15:58:18 +1100

    authorize succes

--- auth.py
+++ auth.py
@@ -1,10 +1,27 @@
-import requests
+# import requests
     
-auth_url = "https://ticktick.com/oauth/authorize"
-params = {
-    "response_type": "code",
-    'scope': 'tasks:write',
-    "client_id": 'wXN3hhG82Nd4pg41jI'
-}
-response = requests.get(auth_url, params=params)
-print(response)
+# auth_url = "https://ticktick.com/oauth/authorize"
+# params = {
+#     "response_type": "code",
+#     'scope': 'tasks:write',
+#     "client_id": 'wXN3hhG82Nd4pg41jI'
+# }
+# response = requests.get(auth_url, params=params)
+# print(response)
+
+from notion_client import Client
+from pprint import pprint
+
+notion_token = 'secret_4EcPpN6LGd8cxncbvF765z7fTBrwqC1goyqAYtEMpw0'
+notion_page_id = '379bd5a210194bf095758068fa1b3fb9'
+
+def main():
+    client = Client(auth=notion_token)
+
+    page_response = client.pages.retrieve(notion_page_id)
+
+    pprint(page_response, indent=2)
+
+if __name__ == '__main__':
+    main()
+          
