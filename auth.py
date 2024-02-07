68a7c81ee4bc5d7a2d20ea19e9f5d867438d3faf
Lucy Lu <52309716+luscarl@users.noreply.github.com>
2024-02-07 23:40:34 +1100

    trying to add blocks to pages

--- auth.py
+++ auth.py
@@ -9,11 +9,13 @@
 # response = requests.get(auth_url, params=params)
 # print(response)
 
-from notion_client import Client
 from pprint import pprint
+from notion_client import Client
 
 notion_token = 'secret_4EcPpN6LGd8cxncbvF765z7fTBrwqC1goyqAYtEMpw0'
-notion_page_id = '379bd5a210194bf095758068fa1b3fb9'
+notion_page_id = '8571975d8d9d49d581eca261c06daf51'
+
+
 
 def main():
     client = Client(auth=notion_token)
@@ -22,6 +24,28 @@ def main():
 
     pprint(page_response, indent=2)
 
+def write_text(client, page_id, text, type):
+    Client.blocks.children.append(
+        block_id=page_id,
+        children=[
+            {
+                "object": "block",
+                "type": type,
+                type: {
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": {
+                                "content": text
+                            }
+                        }
+                    ]
+                }
+            }
+        ]
+    )
+
 if __name__ == '__main__':
     main()
+    write_text('ae8f0b77-72ac-4408-96fc-8b54ea973520', notion_page_id, 'no', 'to_do')
           
